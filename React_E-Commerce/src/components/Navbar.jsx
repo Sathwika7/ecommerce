81927ff7e1d9bbd1b723515df0751124f31fbdce
Sathwika7 <mamidisaisathwika@gmail.com>
2023-09-25 00:03:29 +0530

    new changes

--- React_E-Commerce/src/components/Navbar.jsx
+++ React_E-Commerce/src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-
+import React, {useContext, useEffect, useState} from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { UserContext } from "../pages/UserContextProvider";
+import axios from 'axios';
 const Navbar = () => {
-    const state = useSelector(state => state.handleCart)
+    const state = useSelector(state => state.handleCart);
+    const { user } = useContext(UserContext); 
+    const [cartDetails, setCartDetails] = useState([]);
+    useEffect(() => {
+        // Fetch cart details when the component mounts or when the user changes
+        if (user && user.email) {
+          fetchCartDetails();
+        }
+      }, [user]);
+    
+      const fetchCartDetails = async () => {
+        try {
+          console.log("Fetching cart details for email:", user.email);
+          const response = await axios.get("http://localhost:3000/cart", {
+            params: {
+              email: user.email,
+            },
+          });
+          console.log("Received cart details response:", response.data);
+          setCartDetails(response.data);
+        } catch (error) {
+          console.error("Error fetching cart details:", error);
+        }
+      };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 sticky-top">
             <div className="container">
@@ -27,7 +52,7 @@ const Navbar = () => {
                     <div className="buttons text-center">
                         <NavLink to="/" className="btn btn-outline-dark m-2"><i className="fa fa-sign-in-alt mr-1"></i> Login</NavLink>
                         <NavLink to="/register" className="btn btn-outline-dark m-2"><i className="fa fa-user-plus mr-1"></i> Register</NavLink>
-                        <NavLink to="/cart" className="btn btn-outline-dark m-2"><i className="fa fa-cart-shopping mr-1"></i> Cart ({state.length}) </NavLink>
+                        <NavLink to="/cart" className="btn btn-outline-dark m-2"><i className="fa fa-cart-shopping mr-1"></i> Cart ({cartDetails.length}) </NavLink>
                     </div>
                 </div>
             </div>
